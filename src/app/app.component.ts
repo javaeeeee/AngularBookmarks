6b095a326e9e6ce07e833fbf41362bad8a20cda8
Dmitry Noranovich <javaeeeee@gmail.com>
2016-08-19 08:03:29 -0400

    Added a simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,31 @@
 import {Component} from '@angular/core';
 
+/**
+ * A class to store Bookamrks.
+ */
+export class Bookmark {
+    /**
+     * Bookmark id.
+     */
+    public id: number;
+    /**
+     * Bookmark URL.
+     */
+    public url: string;
+    /**
+     * Bookmark description.
+     */
+    public description: string;
+}
+
 @Component({
     selector: 'my-app',
     templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    bookmark: Bookmark = {
+        id: 1,
+        url: "http://github.com",
+        description: "A lot of great projects."
+    };
+}
