e516b80c823262c1b4992f3d8ee4194585fc5750
Dmitry Noranovich <javaeeeee@gmail.com>
2016-08-19 07:11:12 -0400

    Changed settings to launch in VS Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,11 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {HttpModule, JsonpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
-import {routing, appRoutingProviders} from './app.routing';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        AboutComponent
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule,
-        JsonpModule,
-        routing
-    ],
-    providers: [appRoutingProviders],
+    imports: [BrowserModule],
+    declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
-
-export class AppModule {
-}
+export class AppModule {}
